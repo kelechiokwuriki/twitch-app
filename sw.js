fd0424c08d0564d8be413ed399ec5aaefc12bf73
kelechi okwuriki <kelechiokwuriki@gmail.com>
2017-07-11 05:27:01 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 var cacheName = 'v1';
-
 var cacheFiles = [
     '/',
     'index.html',
@@ -13,74 +12,73 @@ var cacheFiles = [
 
 self.addEventListener('install', function(e){
     console.log("Service worker installed");
-    
+
     e.waitUntil(
-        
+
         caches.open(cacheName).then(function(cache){
-            
+
             console.log("Service worker caching files");
             return cache.addAll(cacheFiles);
         })
     )
-})   
+})
 
 self.addEventListener('activate', function(e){
     console.log("Service worker activated");
-    
+
     e.waitUntil(
-        
+
         caches.keys().then(function(cacheNames){
-            
+
             return Promise.all(cacheNames.map(function(thisCacheName){
-                
+
                 if(thisCacheName !== cacheName){
-                    
+
                     console.log("Service worker removing cached files from", thisCacheName);
                     return caches.delete(thisCacheName);
-                }    
+                }
             }))
-            
+
         })
     );
-});  
+});
 
 
 
 self.addEventListener('fetch', function(e){
     console.log("Service worker fetching", e.request.url);
-    
+
     e.respondWith(
-        
+
         caches.match(e.request).then(function(response){
-            
+
             if(response){
-               
+
                 console.log("Service worker: found in cache", e.request.url);
                 return response;
             }
-            
+
             var requestClone = e.request.clone();
-            
+
             fetch(requestClone)
                 .then(function(response){
-                    
+
                 if(!response){
                     console.log("Service Worker: No response from from fetch");
                     return response;
                 }
-                
+
                 var responseClone = response.clone();
-            
+
                 caches.open(cacheName).then(function(cache){
                     cache.put(e.request, responseClone);
                     return response;
                 });
-            
+
             }).catch(function(err){
-               console.log("Service worker: Error fetching and caching new files");  
+               console.log("Service worker: Error fetching and caching new files");
             })
-            
+
         })
     )
-})  
-
+})
